d6f06ac3d93e9059fa68dcfe9174b63f82b0a74c
Carlos Berrío <98783366+carlosberrio@users.noreply.github.com>
2022-11-30 22:45:18 -0500

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,1 @@
-/* The best code */
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
-}
+
