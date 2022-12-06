c338fe44cfdf0b9369ad740d8af2d9ae4ca5ad41
Carlos Berrio <4634@holbertonschool.com>
2022-12-06 15:19:40 -0500

    task3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
-/* Create a function getStudentIdsSum that returns the sum of all the student ids */
-
 export default function getStudentIdsSum(array) {
   if (Array.isArray(array)) {
     return array.reduce((sum, student) => sum + student.id, 0);
